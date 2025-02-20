290a6fa29bf4de29a9ac366bad2c336e0d23d49f
MasonJEmpey <165834350+MasonJEmpey@users.noreply.github.com>
2025-02-20 16:37:17 -0700

    login adjusted

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -23,10 +23,13 @@ const Login = () => {
 
   const handleSignUp = async () => {
     try {
-      const res = await axios.post(`/api/users/register`, {
-        email,
-        password,
-      });
+      const res = await axios.post(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/users/register`,
+        {
+          email,
+          password,
+        }
+      );
       console.log("User created:", res.data);
       if (res) {
         window.location.href = "/";
@@ -58,9 +61,12 @@ const Login = () => {
       const user = result.user;
 
       // Check if the user exists in backend
-      const checkUserResponse = await axios.post(`/api/users/validate`, {
-        uid: user.uid,
-      });
+      const checkUserResponse = await axios.post(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/users/validate`,
+        {
+          uid: user.uid,
+        }
+      );
 
       if (!checkUserResponse.data.exists) {
         await axios.post(`/api/users/googleregister`, {
